ce913a83dfdbdf750cba5b0ce0f7cfa20b0613d6
swarajjogi <swarajjogi@gmail.com>
2025-12-29 13:21:00 +0530

    Configure API base URL via env variables

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const BASE_URL = process.env.REACT_APP_API_URL || "https://students-backend-ecur.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
